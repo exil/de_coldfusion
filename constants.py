14a1207246505dd1afb1b0bbd070e85072730d85
wenn <nnguyen920@gmail.com>
2013-04-12 00:08:43 -0500

    refacotored varscoper

--- constants.py
+++ constants.py
@@ -1,2 +1,4 @@
 REGION_TAG = "cflinter_region_tag"
 REGION_TAB = "cflinter_region_tab"
+LOCALHOST_VARSCOPER_URL = "http://localhost/varscoper/index.cfm"
+VARSCOPER_REGION = "varscoper_region"
--- function_util.py
+++
@@ -1,8 +0,0 @@
-import sublime
-import sublime_plugin
-
-class FunctionUtil():
-	def findFunctions(self, view):
-		functionSelections = view.find_all("<cffunction ([^\n]*\n+)+(.*?)</cffunction>")
-
-		print functionSelections
